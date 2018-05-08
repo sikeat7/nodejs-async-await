c3d0c6489a1024878669e79dc2cc274d2b13538d
Sikeat <kobsikeat@outlook.com>
2018-05-08 12:49:09 +0700

    Add convertCurrencyAlt using Async await

--- currency-convert.js
+++ currency-convert.js
@@ -15,6 +15,30 @@ const getCountries = (currencyCode) => {
     });
 }
 
-getCountries('CAD').then((country) => {
-    console.log(country);
+const convertCurrency = (from, to, amount) => {
+    let countries;
+    return getCountries(to).then((tempCountries) => {
+        countries = tempCountries;
+        return getExchangeRate(from, to);
+    }).then((rate) => {
+        const exchangeAmount = amount * rate;
+
+        return `${amount} ${from} is worth ${exchangeAmount} ${to}. ${to} can be used in the following countries: ${countries.join(', ')}`;
+    }).catch((e) => console.log(`${to} not available in that country.`));
+}
+
+// Create convertCurrencyAlt as async function
+// Get countries and rate using await and our two function
+// Calculate exchangeAmount
+// Return status string
+const convertCurrencyAlt = async (from, to, amount) => {
+    const countries = await getCountries(to);
+    const rate = await getExchangeRate(from, to);
+    const exchangeAmount = amount * rate;
+
+    return `${amount} ${from} is worth ${exchangeAmount} ${to}. ${to} can be used in the following countries: ${countries.join(', ')}`;
+}
+
+convertCurrencyAlt('CAD', 'USD', 100).then((log) => {
+    console.log(log);
 });
