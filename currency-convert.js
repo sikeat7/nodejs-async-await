82ecb6007f941eb68556c7322ef9e8d6a668811d
Sikeat <kobsikeat@outlook.com>
2018-05-08 13:01:34 +0700

    Add Asycn Await Throw New Error

--- currency-convert.js
+++ currency-convert.js
@@ -3,16 +3,30 @@
 
 const axios = require('axios');
 
-const getExchangeRate = (from, to) => {
-    return axios.get(`http://api.fixer.io/latest?base=${from}`).then((response) => {
-        return response.data.rates[to];
-    });
+//Uanble to get exhcange rate for USD to KHR
+const getExchangeRate = async (from, to) => {
+    try {
+        const response = await axios.get(`http://api.fixer.io/latest?base=${from}`);
+        const rate = response.data.rates[to];
+
+        if (rate) {
+            return rate;
+        } else {
+            throw new Error();
+        }
+    } catch (e) {
+        throw new Error(`Unable to get exhange rate for ${from} and ${to}.`);
+    }
+    
 };
 
-const getCountries = (currencyCode) => {
-    return  axios.get(`https://restcountries.eu/rest/v2/currency/${currencyCode}`).then((response) => {
+const getCountries = async (currencyCode) => {
+    try {
+        const response = await axios.get(`https://restcountries.eu/rest/v2/currency/${currencyCode}`);
         return response.data.map((country) => country.name);
-    });
+    } catch (e) {
+        throw new Error(`Unable to get countries that use ${currencyCode}`);
+    }
 }
 
 const convertCurrency = (from, to, amount) => {
@@ -39,6 +53,8 @@ const convertCurrencyAlt = async (from, to, amount) => {
     return `${amount} ${from} is worth ${exchangeAmount} ${to}. ${to} can be used in the following countries: ${countries.join(', ')}`;
 }
 
-convertCurrencyAlt('CAD', 'USD', 100).then((log) => {
+convertCurrencyAlt('USD', 'EUR', 100).then((log) => {
     console.log(log);
+}).catch((e) => {
+    console.log(e.message);
 });
